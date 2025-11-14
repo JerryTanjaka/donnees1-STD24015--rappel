85cd117ff0a6fdeac4c7dcb62179ba83cc1e36dc
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-11-14 22:06:03 +0300

    feat: query-3 leave details with employee & team

--- donnees1.sql
+++ donnees1.sql
@@ -10,3 +10,9 @@ from employee e
 LEFT JOIN employee_leave l  
 ON e.id = l.employee_id 
 where l.employee_id is null;
+
+--3. Afficher les congés de tel sorte qu’on voie l’id du congé, le début du congé, la fin du congé, le nom & prénom de l’employé qui prend congé et le nom de son équipe.
+SELECT l.id,l.start_date,l.end_date,e.first_name,e.last_name,team.name 
+FROM employee_leave l 
+JOIN employee e ON  e.id = l.employee_id 
+LEFT JOIN team ON e.team_id =team.id
