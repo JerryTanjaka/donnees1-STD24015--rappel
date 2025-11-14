774ff7849bbd35d706c1adf8ce6de5c3f1e22338
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-11-14 22:05:32 +0300

    feat: query-2 employees never on leave

--- donnees1.sql
+++ donnees1.sql
@@ -1,4 +1,12 @@
 
 --1. Afficher l’id, first_name, last_name des employés qui n’ont pas d’équipe.
-SELECT id,first_name,last_name from employee where employee.team_id is null;
+SELECT id,first_name,last_name 
+from employee 
+where employee.team_id is null;
 
+--2. Afficher l’id, first_name, last_name des employés qui n’ont jamais pris de congé de leur vie.
+SELECT e.id, e.first_name, e.last_name 
+from employee e 
+LEFT JOIN employee_leave l  
+ON e.id = l.employee_id 
+where l.employee_id is null;
